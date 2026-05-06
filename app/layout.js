eea757f75385711d5e303bfb3ca9777f42cd161d
rheumify <rheumify@pm.me>
2026-05-05 21:44:00 -0700

    feat: add SVG favicon for Rheumify R logo tab icon

--- app/layout.js
+++ app/layout.js
@@ -207,6 +207,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
